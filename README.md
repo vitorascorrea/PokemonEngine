8d72de0ecd71344b0dd1c5a03b11fd2b3290eaa6
vitorascorrea <vitorascorrea@gmail.com>
2017-11-15 14:40:34 -0200

    readme update

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # PokemonEngine
 
-Engine de batalha Pokémon utilizando regras da Gen7/Smogon.
+Engine de batalha Pokémon utilizando regras da Gen7/Smogon com o objetivo de criar uma Inteligência Artificial que consiga ganhar em alto nível competitivo na plataforma [Pokémon Showdown!](http://play.pokemonshowdown.com/).
 
-**Instruções de Instalação (Ubuntu)**
+##Instruções de Instalação (Ubuntu)
 
 1. Tenha Java 8 instalado.
 
@@ -16,8 +16,23 @@ Engine de batalha Pokémon utilizando regras da Gen7/Smogon.
 
 5. Para executar: `java -classpath .:lib/json-simple-master.jar:lib/postgresql-42.1.4.jar:lib/spark-core-2.7.0.jar:bin engine/Main`
 
-**TODO**
+##Como funciona
 
+Temos duas partes nesse projeto: a **engine**, que simula as mecânicas de batalhas do Pokémon, e o **decision maker**, que escolhe quais ações executar na batalha (simulando um jogador real).
+
+Um conceito importante da **engine** é o conceito de **State**, que é um objeto que guarda o estado atual do jogo: quais Pokémons estão vivos, qual é o Pokémon ativo de cada time, etc.
+
+Existem implementados alguns **decision makers**, mas os mais relevantes são:
+
+ - Random - escolhe um movimento aleatório dentre os movimentos possíveis
+ - Simple Greedy - escolhe o movimento que vai gerar a maior diferença total entre HP dos times
+
+Para implementar um novo **decision maker**, é preciso apenas implementar a interface **DecisionMaker** (`engine/decision/`) que recebe dois parâmetros: um objeto do tipo **State** e um inteiro *1* ou *2* que representa o time do usuário daquele decision maker (importante pois a engine trabalha com esse conceito de ordem entre os times). Exemplos em `engine/Main.java`
+
+##TODO
+
+- [ ] Documentar o código!
+- [ ] Implementar um decision maker humano (com input do usuário)
 - [ ] Itens e seus efeitos (Choice Band, Leftovers, etc.)
 - [ ] Traits e seus efeitos (Levitate, Flash Fire, etc.)
 - [ ] Field Hazards (Stealth Rock, Spikes, etc.)
@@ -26,7 +41,7 @@ Engine de batalha Pokémon utilizando regras da Gen7/Smogon.
 - [ ] Z-moves
 - [ ] Mega Evolution
 
-**API para jogar no Pokémon Showdown!**
+##API para jogar no Pokémon Showdown!
 
 Em uma versão antiga dessa Engine (que era em Ruby), foi feito uma API que se comunicava com o bot [https://github.com/Ecuacion/Pokemon-Showdown-Node-Bot](https://github.com/Ecuacion/Pokemon-Showdown-Node-Bot). 
 
--- engine/Main.java
+++ engine/Main.java
@@ -9,52 +9,44 @@ import java.util.Random;
 
 public class Main {
   public static void main(String[] args){
-
     DecisionMaker p1_decision_maker = new GreedySimple();
-    DecisionMaker p2_decision_maker = new MCTS();
+    DecisionMaker p2_decision_maker = new Random();
     runGame(p1_decision_maker, p2_decision_maker, false);
-    // for (int i = 0; i < 100; i++) {
-      // runGame(p1_decision_maker, p2_decision_maker, true);
-      // new Thread() {
-      //   public void run() {
-      //   }
-      // }.start();
-    // }
   }
 
   public static void runGame(DecisionMaker p1_decision_maker, DecisionMaker p2_decision_maker, boolean simulating) {
     Utils utils = new Utils();
     Game game = new Game();
-    ArrayList<Pokemon> team_p1 = utils.getSampleTeam();
-    ArrayList<Pokemon> team_p2 = utils.getSampleTeam();
+    try {
+      ArrayList<Pokemon> team_p1 = utils.getSampleTeam();
+      ArrayList<Pokemon> team_p2 = utils.getSampleTeam();
 
-    State state = new State(team_p1, team_p2);
-    int count = 0;
-    while (!state.terminalState()) {
-      int p1_move = p1_decision_maker.findNextMove(state, 1);
-      int p2_move = p2_decision_maker.findNextMove(state, 2);
+      State state = new State(team_p1, team_p2);
+      int count = 0;
+      while (!state.terminalState()) {
+        int p1_move = p1_decision_maker.findNextMove(state, 1);
+        int p2_move = p2_decision_maker.findNextMove(state, 2);
 
-      game.turn(state, p1_move, p2_move, simulating, count);
+        game.turn(state, p1_move, p2_move, simulating, count);
 
-      if (state.getActivePokemon(1).hp <= 0 && !state.terminalState()) {
-        int rand1 = new Random().nextInt(state.avaliableActions(1).size());
-        if(!simulating) System.out.println("P1: " + state.avaliableActions(1).get(rand1).name);
-        state.switchActivePokemon(1, state.avaliableActions(1).get(rand1));
+        if (state.getActivePokemon(1).hp <= 0 && !state.terminalState()) {
+          int rand1 = new Random().nextInt(state.avaliableActions(1).size());
+          if(!simulating) System.out.println("P1: " + state.avaliableActions(1).get(rand1).name);
+          state.switchActivePokemon(1, state.avaliableActions(1).get(rand1));
 
+        }
+
+        if (state.getActivePokemon(2).hp <= 0 && !state.terminalState()) {
+          int rand2 = new Random().nextInt(state.avaliableActions(2).size());
+          if(!simulating) System.out.println("P2: " + state.avaliableActions(2).get(rand2).name);
+          state.switchActivePokemon(2, state.avaliableActions(2).get(rand2));
+        }
+
+        count++;
       }
-
-      if (state.getActivePokemon(2).hp <= 0 && !state.terminalState()) {
-        int rand2 = new Random().nextInt(state.avaliableActions(2).size());
-        if(!simulating) System.out.println("P2: " + state.avaliableActions(2).get(rand2).name);
-        state.switchActivePokemon(2, state.avaliableActions(2).get(rand2));
-      }
-
-      count++;
-    }
     System.out.println("P " + state.winnerTeam() + " won!");
-    // try {
-    // } catch (Exception e) {
-    //   System.out.println("error");
-    // }
+    } catch (Exception e) {
+      System.out.println("error");
+    }
   }
 }
